eedd0556dfb364775cbcef8883cdfd77bb7b40e2
Majercsik73 <riasztotnekem@gmail.com>
2022-02-06 16:10:53 +0100

    kialakítás

--- app.js
+++ app.js
@@ -1,5 +1,81 @@
+
+
 let kepek=["sovereign.png", "magyarszentek.png",
 		   "magyarszabadsag.png","aranymeteorit.jpg",
 		   "aranyerme.jpg","aranydukat.png"];
 
 let aktualis = 0;
+
+ReactDOM.render(
+    React.createElement(App),
+    document.getElementById("app-container")
+);
+
+function App(){
+    var [kep, setKep] = React.useState(aktualis);
+    console.log(aktualis);
+    return React.createElement(
+        "div",
+        {
+            style:{
+                textAlign: "center"
+            },
+            className: "border"
+        },
+        React.createElement(
+            "button",
+            {
+                className: "btn btn-primary m-4",
+                style:{
+                    width: "95px"
+                },
+                onClick: ()=>{
+                    if(aktualis > 0){
+                        setKep(aktualis--);
+                        /*document.getElementById("kep").innerHTML = "";
+                        var kep = document.createElement("img");
+                        kep.src = "image/" + kepek[aktualis];
+                        document.getElementById("kep").appendChild(kep);*/
+                    }
+                    console.log(aktualis);
+                }
+            },
+            "Előző"
+        ),
+        React.createElement(
+            "img",
+            {
+                src: kepek[kep],
+                style:{
+                    width: "400px",
+                    height: "200px",
+                    //display: "flex",
+                    //flexDirection: "column"
+                    //marginLeft: "auto",
+                    //marginRight: "auto",
+                },
+            }
+        ),
+        React.createElement(
+            "button",
+            {
+                className: "btn btn-primary m-4",
+                style:{
+                    //float: "right",
+                    width: "95px"
+                },
+                onClick: ()=>{
+                    if(aktualis < 5){
+                        setKep(aktualis++);
+                        /*document.getElementById("kep").innerHTML = "";
+                        var kep = document.createElement("img");
+                        kep.src = "image/" + kepek[aktualis];
+                        document.getElementById("kep").appendChild(kep);*/
+                    }
+                    console.log(aktualis);
+                }
+            },
+            "Következő"
+        ),
+    )
+}
